f1f8498f7069151b60d7ea29c9ac8825f0418227
Kendal Sosa <kendal.alfonso37@gmail.com>
2017-11-12 00:02:07 -0600

    Serializacion del Modelo Usuarios

--- nani/app/usuarios/urls.py
+++ nani/app/usuarios/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import forma_suffix_patterns
+from usuarios import views
 from django.contrib import admin
 
 urlpatterns = [
-
+	# url(r'^usuarios/$', views.UsuarioList.as_view())
+	# Terminar URLs para enviar datos en RestFramework
 ]
