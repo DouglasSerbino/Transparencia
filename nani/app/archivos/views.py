08fb7961ef3f2fd2cdad2c54336b236fad6bd6df
Douglas Serbino <douglas@serbino>
2017-11-12 02:07:45 -0600

    Crud documentos 70%

--- nani/app/archivos/views.py
+++ nani/app/archivos/views.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .forms import DocumentoForm
+from forms import DocumentoForm
+from models import Documento
 
 # Create your views here.
 def documentos(request):
+	documento = Documento.objects.all()
 	if request.method == 'POST':
 		form = DocumentoForm(request.POST)
 
@@ -15,4 +17,4 @@ def documentos(request):
 			form = DocumentoForm()
 	else:
 		form = DocumentoForm()
-	return render(request, "Administracion/documentos.html", {'form':form})
+	return render(request, "Administracion/documentos.html", {'form':form, 'documento':documento})
