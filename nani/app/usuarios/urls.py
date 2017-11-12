9a31d2fda2b4c12a65389b85f0175613d9b557f7
Douglas Serbino <douglas@serbino>
2017-11-11 21:55:14 -0600

    Cambios

--- nani/app/usuarios/urls.py
+++ nani/app/usuarios/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import url
 from django.contrib import admin
-<<<<<<< HEAD
-from app.archivos import views 
-=======
->>>>>>> 5a01debfff5c98c352071349b41160118d38771b
 
 urlpatterns = [
 
