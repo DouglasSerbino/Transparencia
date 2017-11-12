258021ead91e67059b231de82087e71b79900620
Douglas Serbino <douglas@serbino>
2017-11-12 09:44:08 -0600

    Subida modificacion

--- nani/app/archivos/views.py
+++ nani/app/archivos/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from forms import DocumentoForm
 from models import Documento
+
 
 # Create your views here.
 def documentos(request):
@@ -16,3 +19,11 @@ def documentos(request):
 	else:
 		form = DocumentoForm()
 	return render(request, "Administracion/documentos.html", {'form':form, 'documento':documento})
+
+def editardocs(request, id_documento):
+	documento = Documento.objects.get(id=id_documento)
+	form = DocumentoForm(request.POST or None, request.FILES or None, instance=documento)
+	if form.is_valid():
+		form.save()
+		return HttpResponseRedirect(reverse('archivos:principal'))
+	return render(request, 'Administracion/editdocs.html', {'form': form})  
