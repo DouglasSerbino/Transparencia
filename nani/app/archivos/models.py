c9d08608cd53bd9a720e80d85bc44150141ae4ae
Douglas Serbino <douglas@serbino>
2017-11-11 19:05:34 -0600

    Subida de archivos 60%

--- nani/app/archivos/models.py
+++ nani/app/archivos/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+#Librerias para eliminar los documentos del servidor si es necesario
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 from django.db import models
 
 # Create your models here.
+class Documento(models.Model):
+	nombre_documento = models.CharField(max_length=100, unique=True)
+	archivo = models.FileField(upload_to='documentos/')
+
+	class Meta:
+		verbose_name='Documento'
+		verbose_name_plural='Documentos'
+	def __str__(self):
+		return '%s' %(self.nombre_documento)
+
+@receiver(post_delete, sender=Documento)
+def archivo(sender, instance, **kwargs):
+    """ Borra los ficheros de las fotos que se eliminan. """
+    instance.archivo.delete(False)
--- nani/nani/settings.py
+++ nani/nani/settings.py
@@ -126,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Configuracion de subida de archivos
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR, 'media'),
+    )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
